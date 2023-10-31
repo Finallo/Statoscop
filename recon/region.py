029ecd829ca932527d51bca8823ec9fd56efe83f
finallo <finallo360@gmail.Com>
2023-10-31 11:45:57 +0100

    Recon.gg

--- recon/region.py
+++
@@ -1,3 +0,0 @@
-class region():
-    def __init__(self):
-        print("Les regions existantes sont : \n All, north-america, europe, brazil, asia-pacific, korea, japan, latin-america, oceania, mena, game-changers, collegiate")
