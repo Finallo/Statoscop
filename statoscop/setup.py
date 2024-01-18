c15b264f452ef12db4564d87840023476cb934d2
Finallo <finallo360@gmail.com>
2024-01-18 13:27:13 +0100

    commit

--- statoscop/setup.py
+++ statoscop/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Statoscop",
-    version="1.3.1",
+    version="1.3.2",
     author="Finallo",
     author_email="finallo360@gmail.com",
     description="A package to have data of Valorant esport. (League and TFT are coming)",
